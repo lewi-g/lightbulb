7e0aa04fd11771c400555877cf418052b1dffbd2
lewi-g <dr.lewi@gmail.com>
2017-06-15 14:54:02 -0400

    final

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // your code here
+// event listener on click of .js-light-bulb 
+// add class .bulb-on to element clicked
+// remove class from all other elements
+console.log('hello josh');
+$(function(){
+	$('.js-lightbulb').click(function(event) {
+		$('*').find('.bulb-on').removeClass('bulb-on');
+		$(event.target).addClass('bulb-on');
+
+	})
+})
